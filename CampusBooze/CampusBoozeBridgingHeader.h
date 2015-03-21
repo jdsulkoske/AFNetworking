01e3e6571f1c974a2c852a8c69cde4a94ecc0012
jdsulkoske <jdsulkoske@bsu.edu>
2015-03-20 23:41:35 -0400

    First commit

---
+++ CampusBooze/CampusBoozeBridgingHeader.h
@@ -0,0 +1,15 @@
+//
+//  CampusBoozeBridgingHeader.h
+//  CampusBooze
+//
+//  Created by Jake Sulkoske on 3/20/15.
+//  Copyright (c) 2015 Sulk. All rights reserved.
+//
+
+#import <AFNetworking/AFNetworking.h>
+
+#ifndef CampusBooze_CampusBoozeBridgingHeader_h
+#define CampusBooze_CampusBoozeBridgingHeader_h
+
+
+#endif
